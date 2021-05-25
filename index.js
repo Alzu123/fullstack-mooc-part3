9eaf02b9f54a308e046adf2c96b474baa891facc
Neero Lius <neero@lius.fi>
2021-05-25 15:25:08 +0300

    uibuild

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
 
 morgan.token('body', (request, response) => JSON.stringify(request.body))
 const morganConfig = ':method :url :status :res[content-length] - :response-time ms :body'
